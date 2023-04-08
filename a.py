51a03861ed7398f9ee194517f23a6796bad93343
Seongil Heo <tjddlf101@gmail.com>
2023-04-09 02:24:59 +0900

    b

--- a.py
+++ a.py
@@ -5,12 +5,14 @@ global locations, dx, dy, weapon, board, n, m, k, directions, firstPower
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
-
 def getGunInfo():
     for i in range(n):
         a = list(map(int, input().split()))
-        for j in a:
-            board[i].append([j])
+        for j in range(n):
+            if a[j]==0:
+                continue
+            else:
+                board[i][j].append(a[j])
 
 
 def getUserInfo():
@@ -32,9 +34,7 @@ def move(i):
     # 격자 범위 벗어나면
     if nx < 0 or nx >= n or ny < 0 or ny >= n:
         # 정반대로 방향 변경
-        newd = d + 2
-        if newd > 3:
-            newd -= 4
+        newd = (d + 2)%4
 
         nnx = x + dx[newd]
         nny = y + dy[newd]
@@ -43,7 +43,6 @@ def move(i):
         directions[i] = newd
     else:
         locations[i] = nx, ny
-
 
 # 현재 유저, 해당 유저의 x좌표, y좌표
 def isUserExisted(i, user_x, user_y):
@@ -57,81 +56,47 @@ def isUserExisted(i, user_x, user_y):
 
 # 격자에 총이 존재하는지?
 def isGunExisted(x, y):
-    for i in range(len(board[x][y])):
-        if board[x][y][i] == 0:
-            return False
-    return True
-
+    if board[x][y]:
+        return True
+    return False
 
 # 유저번호, 총이 있는 좌표
 def getGun(i, gun_x, gun_y):
     # 놓여져 있는 총과 유저가 가진 총 중에서 더 센 총 획득
-    putGunsMax, userGunsMax = 0, 0
+    putGunsMax, userGun = 0, 0
+    userGun = weapon[i]
     putGunsMax = max(board[gun_x][gun_y])
 
-    # 유저가 갖고있는 총 중에 가장 쎈 총
-    for j in range(len(weapon[i])):
-        if userGunsMax < weapon[i][j]:
-            userGunsMax = weapon[i][j]
-
     # 놓여져 있는 총이 더 쎄다면
-    if putGunsMax > userGunsMax:
-        for g in weapon[i]:
+    if putGunsMax > userGun:
+        # user가 총이 있으면
+        if userGun !=0:
             # 바닥에 내려놓기
-            board[gun_x][gun_y].append(g)
-        weapon[i] = []
+            board[gun_x][gun_y].append(userGun)
 
         # 가장 쎈 총 획득
-        weapon[i].append(putGunsMax)
         board[gun_x][gun_y].remove(putGunsMax)
-
-        if not board[gun_x][gun_y]:
-            board[gun_x][gun_y] = [0]
-
+        weapon[i]=putGunsMax
     # 유저가 가진 총이 더 쎄다면 아무 것도 하지 않음
 
 # 이동한 다음
 def after_move(i):
     x, y = locations[i]
     otherUser = isUserExisted(i, x, y)
-
     # 플레이어 없다면
-    if otherUser == "no":
+    if otherUser=="no":
         # 해당 칸에 총 있음
         if isGunExisted(x, y):
-
-            # 총을 갖고 있지 않다면
-            if len(weapon[i]) == 0:
-                # 총 획득
-                for j in board[x][y]:
-                    weapon[i].append(j)
-
-                # 맵에서 총 지우기
-                board[x][y] = [0]
-
-            # 플레이어가 이미 총 가짐
-            else:
-                getGun(i, x, y)
-
+            getGun(i,x,y)
         # 해당 칸에 총 없으면 패스
 
     # 플레이어가 있다면 싸우기
     else:
-        fight(i, otherUser)
-
-# 유저의 총의 공격력 합 구하기
-def calSumGunPower(user):
-    power = 0
-    for i in range(len(weapon[user])):
-        power += weapon[user][i]
-
-    return power
-
+        fight(i,otherUser)
 
 def fight(user1, user2):
-    power1, power2 = calSumGunPower(user1), calSumGunPower(user2)
-    user1_info = firstPower[user1] + power1
-    user2_info = firstPower[user2] + power2
+    user1_info = firstPower[user1] + weapon[user1]#power1
+    user2_info = firstPower[user2] + weapon[user2]#power2
 
     if user1_info > user2_info:
         winner = user1
@@ -147,59 +112,54 @@ def fight(user1, user2):
         winner = user2
         loser = user1
 
+    # loser action
+    loser_action(loser, locations[loser][0], locations[loser][1])
+    
+    # winner action
     # 각 플레이어의 초기 능력치와 갖고있는 총의 공격력의 합의 차이만큼 포인트 획득
     points[winner] += abs(user1_info - user2_info)
-    loser_action(loser, locations[loser][0], locations[loser][1])
-    getGun(winner, locations[winner][0], locations[winner][1])
-
-
+    if isGunExisted(*locations[winner]):
+        getGun(winner,*locations[winner])
+        
 # 싸움에서 진 플레이어
 def loser_action(loser, x, y):
     # 본인이 가진 총을 해당 격자에 모두 내려놓기
-    for i in weapon[loser]:
-        board[x][y].append(i)
-        weapon[loser] = []
+    if weapon[loser]!=0:
+        board[x][y].append(weapon[loser])
+        weapon[loser]=0
 
     # 이동
     userx, usery = locations[loser]
     d = directions[loser]
-    nx = userx + dx[d]
+    nx = userx + dx[d] # 이동할 곳
     ny = usery + dy[d]
 
     # 다른 플레이어 존재하거나 격자 벗어나면
-    if isUserExisted(loser, nx, ny) != "no" or nx < 0 or nx >= n or ny < 0 or ny >= n:
-        # 오른쪽으로 90도 회전
-        for i in range(1, 4):
-            newd = d + i
-            if newd > 3:
-                newd -= 4
-            nnx = userx + dx[newd]
-            nny = usery + dy[newd]
+    while isUserExisted(loser, nx, ny) !="no" or nx < 0 or nx >= n or ny < 0 or ny >= n:
+        # 오른쪽으로 90도씩 회전
+        d=(d+1)%4
+        nx=userx+dx[d]
+        ny=usery+dy[d]
 
-            if isUserExisted(loser, nnx, nny) == "no":
-                locations[loser] = nnx, nny
-                directions[loser] = newd
-                break
-
-    else:
-        locations[loser] = nx, ny
-
+    # 이동해야함
+    locations[loser] = nx, ny
+    directions[loser]=d
+    
     # 해당 칸에 총이 있다면
-    movedUserX, movedUserY = locations[loser]
-    if isGunExisted(movedUserX, movedUserY):
-        getGun(loser, movedUserX, movedUserY)
-
+    if isGunExisted(*locations[loser]):
+        getGun(loser,*locations[loser])
 
 n, m, k = map(int, input().split())
-board = [[] for _ in range(n)]
+board = [[[] for _ in range(n)] for _ in range(n)]
 locations = []
 directions = []
-weapon = [[] for _ in range(m)]
+weapon = [0]*m
 firstPower = []
 points = [0] * m
 
 getGunInfo()
 getUserInfo()
+
 cur = 0
 
 while cur < k:
